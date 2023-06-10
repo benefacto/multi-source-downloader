7cfb7d528236958fd7511c5a43cfff55d70d4fba
Ben Walker <ben.walker18@gmail.com>
2023-06-10 17:11:09 +0100

    Get etag check working again with decent speed

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 const (
 	fileURL = "https://zenodo.org/record/4435114/files/supplement.csv?download=1"
 	maxRetries = 3
-	numberOfChunks = 4
+	numberOfChunks = 3
 )
 
 func main() {
@@ -27,6 +27,8 @@ func main() {
 	if err != nil {
 		errorLogger.Println("Error making HTTP HEAD request to", fileURL, err)
 		return
+	} else {
+		infoLogger.Println("Successfully made HTTP HEAD request to", fileURL)
 	}
 
 	etag := resp.Header.Get("Etag")
@@ -42,6 +44,7 @@ func main() {
 
 	var wg sync.WaitGroup
 	md5Hash := md5.New()
+	tempFiles := make([]string, numberOfChunks) // to store names of temporary files
 
 	for currentChunkIndex := 0; currentChunkIndex < numberOfChunks; currentChunkIndex++ {
 		wg.Add(1)
@@ -59,10 +62,10 @@ func main() {
 				client := &http.Client{}
 				req, err := http.NewRequest("GET", fileURL, nil)
 				if err != nil {
-					errorLogger.Println("Error making HTTP GET request to", fileURL, err)
+					errorLogger.Println("Chunk", currentChunkIndex, "had an error making HTTP GET request to", fileURL, err)
 					return
 				} else {
-					infoLogger.Println("Successfuly HTTP GET request made to", fileURL)
+					infoLogger.Println("Chunk", currentChunkIndex, "successfully made HTTP GET request to", fileURL)
 				}
 
 				rangeHeader := fmt.Sprintf("bytes=%d-%d", start, end)
@@ -70,7 +73,7 @@ func main() {
 
 				resp, err := client.Do(req)
 				if err != nil {
-					errorLogger.Println("Error making HTTP Range request to", fileURL, err)
+					errorLogger.Println("Chunk", currentChunkIndex, "had an error making HTTP Range request to", fileURL, err)
 					if retries < maxRetries - 1 {
 						infoLogger.Println("Retrying chunk", currentChunkIndex, "...")
 						time.Sleep(time.Second * time.Duration(retries+1)) // exponential back-off
@@ -80,73 +83,61 @@ func main() {
 				}
 
 				tmpFileName := fmt.Sprintf("tmpfile_%d", currentChunkIndex)
+				tempFiles[currentChunkIndex] = tmpFileName // save name of temp file
 				tmpFile, err := os.Create(tmpFileName)
 				if err != nil {
-					errorLogger.Println("Error creating temporary file", tmpFileName, err)
+					errorLogger.Println("Chunk", currentChunkIndex, "had an error creating temporary file", tmpFileName, err)
 					return
 				} else {
-					infoLogger.Println("Created temporary file", tmpFileName)
+					infoLogger.Println("Chunk", currentChunkIndex, "successfully created temporary file", tmpFileName)
 				}
 
 				_, err = io.Copy(tmpFile, resp.Body)
 				if err != nil {
-					errorLogger.Println("Error copying response body to temporary file", tmpFileName, err)
-					tmpFile.Close()
-					os.Remove(tmpFileName) // clean up
-					if retries < maxRetries - 1 {
-						infoLogger.Println("Retrying chunk", currentChunkIndex, "...")
-						time.Sleep(time.Second * time.Duration(retries+1)) // exponential back-off
-						continue
-					}
+					errorLogger.Println("Chunk", currentChunkIndex, "had an error writing to temporary file", tmpFileName, err)
 					return
 				} else {
-					infoLogger.Println("Copied response body to temporary file", tmpFileName)
+					infoLogger.Println("Chunk", currentChunkIndex, "successfully wrote to temporary file", tmpFileName)
 				}
+
 				resp.Body.Close()
-				tmpFile.Seek(0, 0) // reset file pointer to beginning
-
-				partBytes, err := io.ReadAll(tmpFile)
-				if err != nil {
-					errorLogger.Println("Error reading from temporary file", tmpFileName, err)
-					return
-				} else {
-					infoLogger.Println("Read temporary file to write hash", tmpFileName)
-				}
-
-				md5Hash.Write(partBytes) // update hash with file part
-
 				tmpFile.Close()
-				os.Remove(tmpFileName) // remove temp file
 				break
 			}
+
 			if err != nil {
-				errorLogger.Printf("Error downloading chunk %d: %v\n", currentChunkIndex, err)
-			} else {
-				infoLogger.Printf("Successfully downloaded chunk %d", currentChunkIndex)
+				errorLogger.Println("Chunk", currentChunkIndex, "had an error after retries", err)
 			}
 		}(currentChunkIndex)
 	}
 
 	wg.Wait()
 
-	t := time.Now()
-	timestamp := t.Format("20060102_150405")
-	fileName := fmt.Sprintf("output_%s.csv", timestamp)
-	file, err := os.Create(fileName)
-	if err != nil {
-		errorLogger.Println("Error creating file", fileName, err)
-		return
-	} else {
-		infoLogger.Println("Created file", fileName)
-	}
-	defer file.Close()
+	for _, tmpFileName := range tempFiles {
+		tmpFile, err := os.Open(tmpFileName)
+		if err != nil {
+			errorLogger.Println("Error opening temporary file", tmpFileName, err)
+			return
+		} else {
+			infoLogger.Println("Successfully opened temporary file", tmpFileName)
+		}
 
-	// TODO: Get this working
-	// File hash: "db330cb3a5fbddee1f36f77f9f83670e" does not match the ETag: "md5:aab5e2178af3844b1ab801112ab748f6"
-	calculatedMd5 := fmt.Sprintf(`"%x"`, md5Hash.Sum(nil))
-	if calculatedMd5 == etag {
-		infoLogger.Println("File hash:", calculatedMd5 ,"matches the ETag:", etag)
+		_, err = io.Copy(md5Hash, tmpFile)
+		if err != nil {
+			errorLogger.Println("Error hashing content of temporary file", tmpFileName, err)
+			return
+		} else {
+			infoLogger.Println("Successfully hashed content of temporary file", tmpFileName)
+		}
+
+		tmpFile.Close()
+		os.Remove(tmpFileName)
+	}
+
+	checksum := fmt.Sprintf(`"md5:%x"`, md5Hash.Sum(nil))
+	if checksum != etag {
+		warningLogger.Println("MD5 checksum", checksum, "does not match ETag", etag)
 	} else {
-		warningLogger.Println("File hash:", calculatedMd5 ,"does not match the ETag:", etag)
+		infoLogger.Println("MD5 checksum", checksum, "matches ETag", etag)
 	}
 }
